9920c57bd8b95c341277b4e6a220bdcd37f60fe9
Gribnoff <quinvex88@gmail.com>
2019-06-01 22:08:26 +0300

    lesson 2 task 3 complete

--- src/lesson2/Solution.java
+++ src/lesson2/Solution.java
@@ -15,5 +15,13 @@ public class Solution {
         for (int i = 0; i < array2.length; i++) {
             array2[i] = i * 3;
         }
+
+        //Задание №3
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i] < 6)
+                array3[i] *= 2;
+        }
     }
 }
